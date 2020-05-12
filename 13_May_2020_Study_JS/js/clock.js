90f5cdccf157411aeae56903607cd08ee3023e95
zhimin8 <hajimin2006@naver.com>
2020-05-13 03:16:56 +0900

    이름까지 완성함

---
+++ 13_May_2020_Study_JS/js/clock.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector(".clock");
+    clockText = clockContainer.querySelector(".clock_text");
+
+    //현재시간을 알아내자
+    function getTime(){
+        const time = new Date();
+        // hour
+        const hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        //문자열과 변수명을 결합 : 백틱
+        const now = `${hours<10 ? `0${hours}` : hours } : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`; 
+        
+
+        clockText.innerHTML = now;
+    }
+
+function init(){
+    getTime();
+    setInterval(getTime,1000); //1초마다 한번씩 새로고침
+}
+init(); //즉시 실행 함수랑은 다르다. init 메소드 안에 순서를 컨트롤 할 수 있다. 나만의 signiture 함수
---
+++ 13_May_2020_Study_JS/js/hello.js
@@ -0,0 +1,57 @@
+const nameContainer = document.querySelector(".myName");
+
+function drawName(name){
+    nameContainer.innerHTML = "";
+    const drawName = document.createElement("span");
+    drawName.className = "name_text";
+    drawName.innerHTML = `Hello ! ${name} !`;
+    nameContainer.appendChild(drawName);
+
+}
+
+ 
+// 콜백함수를 밖으로 빼서 관리하는 게 좋음
+function handleSubmit(event){
+    event.preventDefault(); //이벤트버블링의 외출금지화. refresh 못하게 하는것.
+    const form = event.target; // target : 현재 이벤트가 일어나는 장소
+    const input = form.querySelector("input");
+    const name = input.value;
+    localStorage.setItem("username", name);
+    drawName(name);
+
+}
+
+function drawInput(){
+    const input = document.createElement("input"); //input태그 요소를 만들어
+    // input의 요소를 저장해줘
+    input.type = "text";
+    input.className = "input_name";
+    input.placeholder = "Type your name here!";
+    
+    const form = document.createElement("form");
+
+    form.addEventListener("submit", handleSubmit);
+    form.appendChild(input); // form에 input이 붙음 
+    nameContainer.appendChild(form); // div에 form이 붙음
+}
+
+function checkName(){
+    //너 로컬스토리지에 이름이 등록되어 잇니? 확인하자 key값 (string)으로 가져와
+    const name = localStorage.getItem("username");
+    if(name === null ){
+        // 처음 온 사람
+        // 이름을 물어보고
+        drawInput();
+        // username이라는 키값으로 받은 이름을 저장하고
+        // innerHTML로 그 이름을 보여준다
+    } else{
+        // 왔던 사람
+        drawName(name);
+        // username 키값으로 저장된 이름을 가져와서 innerHTML로 보여준다.
+    }
+}
+
+function init(){
+    checkName();
+}
+init();
